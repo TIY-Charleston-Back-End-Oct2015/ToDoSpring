9cf91e649681f686fc4e110ad5e595b97e0afa1f
holdenhughes <holdenmh121489@gmail.com>
2015-11-18 10:54:10 -0500

    Comments

--- src/main/java/com/theironyard/controllers/ToDoSpringController.java
+++ src/main/java/com/theironyard/controllers/ToDoSpringController.java
@@ -23,6 +23,7 @@ public class ToDoSpringController {
         return toDoItems.findAll();
     }
 
+    //this is a route for toDoItems
     @RequestMapping("/add-item")
     public void addItem(HttpServletResponse response, String text) throws IOException {
         response.sendRedirect("/");
